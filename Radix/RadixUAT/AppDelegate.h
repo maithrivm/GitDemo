0690d910a1290d41bf06e0d28bb57a4337f80bc2
Maithri VM <maithri.vm@EMACBOOK-35.local>
2014-11-13 11:54:24 +0530

    Committing the initial version

---
+++ Radix/RadixUAT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RadixUAT
+//
+//  Created by Maithri VM on 13/11/14.
+//  Copyright (c) 2014 Maithri VM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
