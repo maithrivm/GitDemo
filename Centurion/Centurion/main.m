622a0dcfae84f073405324f336dddd9b9c5d67a2
Maithri VM <maithri.vm@EMACBOOK-35.local>
2014-11-13 14:01:56 +0530

    Adding initial copy from Xcode 6.1

---
+++ Centurion/Centurion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Centurion
+//
+//  Created by Maithri VM on 13/11/14.
+//  Copyright (c) 2014 Maithri VM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
